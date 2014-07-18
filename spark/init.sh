3498361ab40a10450e95857aca13ba03ea54814e
BillK <wvkehoe@gmail.com>
2014-07-18 13:53:25 -0400

    Fix position of custom_tgz logic

--- spark/init.sh
+++ spark/init.sh
@@ -7,13 +7,8 @@ if [ -d "spark" ]; then
   return
 fi
 
-# Custom Spark tgz  ?
-if [[ "x$SPARK_CUSTOM_TGZ" != "x" ]]
-then
-  wget $SPARK_CUSTOM_TGZ
-
 # Github tag:
-elif [[ "$SPARK_VERSION" == *\|* ]]
+if [[ "$SPARK_VERSION" == *\|* ]]
 then
   mkdir spark
   pushd spark
@@ -28,62 +23,70 @@ then
   popd
 
 # Pre-packaged spark version:
-else 
-  case "$SPARK_VERSION" in
-    0.7.3)
-      if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
-        wget http://s3.amazonaws.com/spark-related-packages/spark-0.7.3-prebuilt-hadoop1.tgz
-      else
-        wget http://s3.amazonaws.com/spark-related-packages/spark-0.7.3-prebuilt-cdh4.tgz
-      fi
-      ;;    
-    0.8.0)
-      if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
-        wget http://s3.amazonaws.com/spark-related-packages/spark-0.8.0-incubating-bin-hadoop1.tgz
-      else
-        wget http://s3.amazonaws.com/spark-related-packages/spark-0.8.0-incubating-bin-cdh4.tgz
-      fi
-      ;;    
-    0.8.1)
-      if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
-        wget http://s3.amazonaws.com/spark-related-packages/spark-0.8.1-incubating-bin-hadoop1.tgz
-      else
-        wget http://s3.amazonaws.com/spark-related-packages/spark-0.8.1-incubating-bin-cdh4.tgz
-      fi
-      ;;    
-    0.9.0)
-      if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
-        wget http://s3.amazonaws.com/spark-related-packages/spark-0.9.0-incubating-bin-hadoop1.tgz
-      else
-        wget http://s3.amazonaws.com/spark-related-packages/spark-0.9.0-incubating-bin-cdh4.tgz
-      fi
-      ;;
-    0.9.1)
-      if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
-        wget http://s3.amazonaws.com/spark-related-packages/spark-0.9.1-bin-hadoop1.tgz
-      else
-        wget http://s3.amazonaws.com/spark-related-packages/spark-0.9.1-bin-cdh4.tgz
-      fi
-      ;;
-    1.0.0)
-      if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
-        wget http://s3.amazonaws.com/spark-related-packages/spark-1.0.0-bin-hadoop1.tgz
-      else
-        wget http://s3.amazonaws.com/spark-related-packages/spark-1.0.0-bin-cdh4.tgz
-      fi
-      ;;
-    1.0.1)
-      if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
-        wget http://s3.amazonaws.com/spark-related-packages/spark-1.0.1-bin-hadoop1.tgz
-      else
-        wget http://s3.amazonaws.com/spark-related-packages/spark-1.0.1-bin-cdh4.tgz
-      fi
-      ;;
-    *)
-      echo "ERROR: Unknown Spark version"
-      return
-  esac
+else
+  # Custom Spark tgz  ?
+  if [[ "x$SPARK_CUSTOM_TGZ" != "x" ]]
+  then
+    echo "Getting custom Spark tgz archive from $SPARK_CUSTOM_TGZ ..."
+    wget $SPARK_CUSTOM_TGZ
 
+  else
+    case "$SPARK_VERSION" in
+      0.7.3)
+        if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
+          wget http://s3.amazonaws.com/spark-related-packages/spark-0.7.3-prebuilt-hadoop1.tgz
+        else
+          wget http://s3.amazonaws.com/spark-related-packages/spark-0.7.3-prebuilt-cdh4.tgz
+        fi
+        ;;    
+      0.8.0)
+        if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
+          wget http://s3.amazonaws.com/spark-related-packages/spark-0.8.0-incubating-bin-hadoop1.tgz
+        else
+          wget http://s3.amazonaws.com/spark-related-packages/spark-0.8.0-incubating-bin-cdh4.tgz
+        fi
+        ;;    
+      0.8.1)
+        if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
+          wget http://s3.amazonaws.com/spark-related-packages/spark-0.8.1-incubating-bin-hadoop1.tgz
+        else
+          wget http://s3.amazonaws.com/spark-related-packages/spark-0.8.1-incubating-bin-cdh4.tgz
+        fi
+        ;;    
+      0.9.0)
+        if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
+          wget http://s3.amazonaws.com/spark-related-packages/spark-0.9.0-incubating-bin-hadoop1.tgz
+        else
+          wget http://s3.amazonaws.com/spark-related-packages/spark-0.9.0-incubating-bin-cdh4.tgz
+        fi
+        ;;
+      0.9.1)
+        if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
+          wget http://s3.amazonaws.com/spark-related-packages/spark-0.9.1-bin-hadoop1.tgz
+        else
+          wget http://s3.amazonaws.com/spark-related-packages/spark-0.9.1-bin-cdh4.tgz
+        fi
+        ;;
+      1.0.0)
+        if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
+          wget http://s3.amazonaws.com/spark-related-packages/spark-1.0.0-bin-hadoop1.tgz
+        else
+          wget http://s3.amazonaws.com/spark-related-packages/spark-1.0.0-bin-cdh4.tgz
+        fi
+        ;;
+      1.0.1)
+        if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
+          wget http://s3.amazonaws.com/spark-related-packages/spark-1.0.1-bin-hadoop1.tgz
+        else
+          wget http://s3.amazonaws.com/spark-related-packages/spark-1.0.1-bin-cdh4.tgz
+        fi
+        ;;
+      *)
+        echo "ERROR: Unknown Spark version"
+        return
+    esac
+  fi
+  
   echo "Unpacking Spark"
   tar xvzf spark-*.tgz > /tmp/spark-ec2_spark.log
   rm spark-*.tgz
